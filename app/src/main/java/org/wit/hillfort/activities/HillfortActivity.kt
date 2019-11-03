c2fd0ee622c845f079883e8efdca48cb1e886e34
ceriwalsh51 <ceriannewalsh51@gmail.com>
2019-11-03 18:48:05 +0000

    ImplementingAdapters
    The app can now  display the hillforts and its info, and direct the user to a different screen to add a new hillfort

---
+++ app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
@@ -0,0 +1,42 @@
+package org.wit.hillfort.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_hillfort.*
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
+import org.jetbrains.anko.toast
+import org.wit.hillfort.R
+import org.wit.hillfort.main.MainApp
+import org.wit.hillfort.models.HillfortModel
+
+class HillfortActivity : AppCompatActivity(),AnkoLogger {
+
+    var hillfort = HillfortModel()
+    lateinit var app : MainApp
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hillfort)
+        app = application as MainApp
+
+        btnAdd.setOnClickListener() {
+            hillfort.title = hillfortTitle.text.toString()
+            hillfort.description = description.text.toString()
+
+            if(hillfort.title.isNotEmpty()) {
+                app.hillforts.add(hillfort.copy())
+                info("add Button Pressed: $hillfort")
+                for(i in app.hillforts.indices) {
+                    info("Hillfort[$i]:${app.hillforts[i]}")
+                }
+                setResult(AppCompatActivity.RESULT_OK)
+                finish()
+            }
+            else {
+                toast("Please Enter a title")
+            }
+
+        }
+    }
+}
