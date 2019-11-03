7fec5746258ed32d665c3abd9756ae282756668c
ceriwalsh51 <ceriannewalsh51@gmail.com>
2019-11-03 20:08:53 +0000

    Models
    The app can now update a entry, rather than create a new one

--- app/src/main/java/org/wit/hillfort/activities/HillfortListActivity.kt
+++ app/src/main/java/org/wit/hillfort/activities/HillfortListActivity.kt
@@ -1,18 +1,18 @@
 package org.wit.hillfort.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_hillfort_list.*
-import kotlinx.android.synthetic.main.card_placemark.view.*
+import org.jetbrains.anko.intentFor
 import org.jetbrains.anko.startActivityForResult
 import org.wit.hillfort.R
 import org.wit.hillfort.main.MainApp
 import org.wit.hillfort.models.HillfortModel
 
-class HillfortListActivity : AppCompatActivity() {
+class HillfortListActivity : AppCompatActivity(), HillfortListener {
     lateinit var app: MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,7 +25,7 @@ class HillfortListActivity : AppCompatActivity() {
 
         val layoutManager = LinearLayoutManager(this)
         recyclerView.layoutManager = layoutManager
-        recyclerView.adapter = HillfortAdapter(app.hillforts)
+        recyclerView.adapter = HillfortAdapter(app.hillforts.findAll(), this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?):Boolean {
@@ -39,33 +39,13 @@ class HillfortListActivity : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
-}
 
-class HillfortAdapter constructor(private var hillforts: List<HillfortModel>) :
-    RecyclerView.Adapter<HillfortAdapter.MainHolder>() {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
-        return MainHolder(
-            LayoutInflater.from(parent?.context).inflate(
-                R.layout.card_placemark,
-                parent,
-                false
-            )
-        )
+    override fun onHillfortClick(hillfort: HillfortModel) {
+        startActivityForResult(intentFor<HillfortActivity>().putExtra("hillfort_edit", hillfort), 0)
     }
 
-    override fun onBindViewHolder(holder: MainHolder, position: Int) {
-        val hillfort = hillforts[holder.adapterPosition]
-        holder.bind(hillfort)
-    }
-
-    override fun getItemCount(): Int = hillforts.size
-
-    class MainHolder constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-        fun bind(hillfort: HillfortModel) {
-            itemView.hillfortTitle.text = hillfort.title
-            itemView.description.text = hillfort.description
-        }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        recyclerView.adapter?.notifyDataSetChanged()
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
